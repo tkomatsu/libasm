22ce7e9e2ff04ab2e6b775c08f8017823b3912c6
tkomatsu <tkomatsu@student.42tokyo.jp>
2021-01-06 12:14:53 +0900

    fix: modify main
    テストコードを整理

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: tkomatsu <tkomatsu@student.42tokyo.jp>     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/01/01 16:08:17 by tkomatsu          #+#    #+#              #
-#    Updated: 2021/01/04 20:33:04 by tkomatsu         ###   ########.fr        #
+#    Updated: 2021/01/06 11:03:10 by tkomatsu         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -23,9 +23,9 @@ CFLAGS = -Wall -Wextra -Werror
 
 SRC_DIR = src/
 
-FILES = ft_strlen.s
-#		ft_strcpy.s \
-		ft_strcmp.s \
+FILES = ft_strlen.s \
+		ft_strcpy.s
+#		ft_strcmp.s \
 		ft_write.s \
 		ft_read.s \
 		ft_strdup.s
@@ -36,9 +36,9 @@ OBJS = $(SRCS:.s=.o)
 all: $(NAME)
 
 $(NAME): $(OBJS)
-	$(AR) $(ARFLAGS) $@ $<
+	$(AR) $(ARFLAGS) $@ $(OBJS)
 
-.s.o:
+%.o: %.s
 	$(AS) $(ASFLAGS) -o $@ $<
 
 clean:
@@ -48,6 +48,10 @@ fclean:
 	rm -f $(OBJS) $(NAME) asm_test
 
 re: fclean all
+
+debug: ASFLAGS += -g
+debug: CFLAGS += -g
+debug: fclean main
 
 main: $(NAME)
 	$(CC) $(CFLAGS) -o asm_test main.c $<
--- main.c
+++ main.c
@@ -6,20 +6,48 @@
 /*   By: tkomatsu <tkomatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/04 17:47:26 by tkomatsu          #+#    #+#             */
-/*   Updated: 2021/01/04 20:36:57 by tkomatsu         ###   ########.fr       */
+/*   Updated: 2021/01/06 12:13:51 by tkomatsu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
 
-#define STRLEN(x)	printf("	src = \"%s\": org = %d, asm = %d\n", x, (int)strlen(x), ft_strlen(x));
+size_t	ft_strlen(const char *str);
+char	*ft_strcpy(char* dst, const char *src);
+int		ft_strcmp(const char *s1, const char *s2);
+ssize_t	ft_write(int fildes, const void *buf, size_t nbyte);
+ssize_t	ft_read(int fildes, void *buf, size_t nbyte);
+char	*ft_strdup(const char *s1);
 
-int		ft_strlen(char const *str);
+int		test_strlen(char const *str)
+{
+	size_t	i, j;
+
+	i = strlen(str);
+	j = ft_strlen(str);
+	if (i == j)
+		printf("\033[32m[OK]\033[39m ");
+	else
+		printf("\033[31m[NG]\033[39m ");
+	return ((int)(i == j));
+}
 
 int		main(void)
 {
-	puts("ft_strlen test");
-	STRLEN("Hello, world!");
+	char	*src[6] = {"foo", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tristique dui at tellus blandit vulputate. In hac habitasse platea dictumst. In a nibh", "", "\n", "\n\n", "safwe11234{ewrq1231"};
+	int		ret[6];
+
+	/* STRLEN TEST */
+	puts("--------------------------------------------------------------------------------");
+	puts("FT_STRLEN TEST :");
+	for (int i = 0; i < 6; i++)
+		ret[i] = test_strlen((const char*)src[i]);
+	puts("");
+	for (int i = 0; i < 6; i++)
+		if (!ret[i])
+			printf("NG: %s\n", src[i]);
+	puts("--------------------------------------------------------------------------------");
+
 	return (0);
 }
